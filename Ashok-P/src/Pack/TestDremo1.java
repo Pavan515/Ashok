52e1ad31a462368e7853bb844633bcbedc2dcf27
pavan <admin@Q60>
2017-12-11 17:36:02 +0530

    Package created

---
+++ Ashok-P/src/Pack/TestDremo1.java
@@ -0,0 +1,13 @@
+package Pack;
+
+import org.testng.annotations.Test;
+
+public class TestDremo1 
+{
+	@Test
+	public void tres()
+	{
+		System.out.println("test 1");
+	}
+
+}
---
+++ Ashok-P/src/Pack/TestDremo2.java
@@ -0,0 +1,13 @@
+package Pack;
+
+import org.testng.annotations.Test;
+
+public class TestDremo2 
+{
+	@Test
+	public void tres()
+	{
+		System.out.println("test 2");
+	}
+
+}
